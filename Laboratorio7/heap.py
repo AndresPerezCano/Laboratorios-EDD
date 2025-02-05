337c39b82ce81545e7220800241b38ad3a93551a
AndresPerezCano <aperezcan@unal.edu.co>
2025-02-05 00:00:00 -0500

    lab 7

--- Laboratorio7/heap.py
+++ Laboratorio7/heap.py
@@ -6,10 +6,11 @@ class HEAP:
         self._size = capacidad
 
     def size(self):
-        return len(self._size)
+        return len(self._A)
 
     def parent(self,i):
         return int(-(-(i/2)//1) - 1)
+    
     def left(self,𝑖):
        return 2*i+1
     
@@ -19,10 +20,11 @@ class HEAP:
     def max_heapify(self,i):
         l = self.left(i)
         r = self.right(i)
-        if l < self._size and self._A[l] > self._A[i]:
+        largest = i
+        if l < self.size() and self._A[l] > self._A[i]:
             largest = l
-        else:
-            largest = i
+        #else:
+            #largest = i
         if r < self._size and self._A[r] > self._A[largest]:
             largest = r
         if largest != i:
@@ -38,8 +40,8 @@ class HEAP:
             self.max_heapify(indice)
             indice -= 1
     
-    def heap_sort(self,A):
-        self.build_max_heap(A)
+    def heap_sort(self):
+        self.build_max_heap(self._A)
         heap_size = len(self._A)
         indice = len(self._A)
         tempL = []
@@ -65,18 +67,18 @@ class HEAP:
 
 if __name__ == "__main__":
     heap1 = HEAP(14)
-    A = [-3,4,5,23,53,65,95,96,97,98,99,34,11,23]
+    A = [-3,4,5,23,53,65,95,96,97,98,99,34,11,70]
     #[16,4,10,14,7,9,3,2,8,1]
-    print(A)
+    #print(A)
     heap1.setA(A)
-    heap1.max_heapify(2)
-    print(heap1.getA())
-
+    heap1.max_heapify(1)
+    #print(heap1.getA())
     heap100 = HEAP(9)
     A100 = random.sample(range(16),9)
     #print(A100)
     heap100.setA(A100)
     heap100.max_heapify(1)
+    
     #print(heap100.getA())
     #heap100 = HEAP(20)
     #A100 = [random.randint(-100,100) for _ in range(20)]}
@@ -86,19 +88,15 @@ if __name__ == "__main__":
     #heap100.max_heapify(2)
     #print(heap100.getA())
     
-    
-    #heap2 = HEAP(10)
-    #A2 = [4,1,3,2,16,9,10,14,8,7]
-    #print(A2)
-    #heap2.build_max_heap(A2)
-    #print(heap2._A)
-    #heap3 = HEAP(10)
-    #A3 = [16,14,10,8,7,9,3,2,4,1]
-    #print(A3)
-    #heap3.heap_sort(A3)
-    #print(heap3._A)
+    print("build-max-heap:\n")
+    heap2 = HEAP(20)
+    A = random.sample(range(-100,100),20)
+    print(A)
+    heap2.build_max_heap(A)
+    print(heap2.getA())
+    print("__________________________________________________________________________________________________________________________\n")
+    print("heap-sort:\n")
+    print(A)
+    heap2.heap_sort()
+    print(heap2.getA())
 
-
-
-
-   
